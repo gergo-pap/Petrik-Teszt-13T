df7437b3c6be925638f2faf36f7a585dc0938f80
Pap Gergő <p.gergo99@gmail.com>
2018-09-24 14:30:15 +0200

    első feladat

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,15 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            int [] szam = new int [10];
+            string bemenet;
+            Console.WriteLine("Írj be 10 számot: ");
+            for (int i = 0; i < 9; i++)
+            {
+                bemenet = Console.ReadLine();
+                szam [i ]= int.Parse(bemenet);
+            }
+            Console.ReadKey();
         }
     }
 }
