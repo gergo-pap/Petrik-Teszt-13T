740bc952495c3e3035e4ebd5f31fda7adcdffd67
Pap Gergő <p.gergo99@gmail.com>
2018-09-24 14:38:29 +0200

    negyedik feladat

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,32 +10,37 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Írd be hány számot kérjen be a program: ");
-            int darab = int.Parse(Console.ReadLine());
-            int [] tomb = new int [darab];
-            string bemenet;
-            Console.WriteLine("Írj be {0} darab számot: ", darab);
-            for (int i = 0; i < darab; i++)
-            {
-                bemenet = Console.ReadLine();
-                tomb [i ]= int.Parse(bemenet);
-            }
-            int max = tomb[0];
-            int min = tomb[0];
-            for (int i = 0; i < darab; i++)
-            {
-                if (tomb[i] > max)
-                {
-                    max = tomb[i];
-                }
-                else if (tomb[i] < max)
-                {
-                    min = tomb[i];
-                }
-            }
-            Console.WriteLine("Max elem: {0}, min elem: {1}",max,min);
 
-
+            try
+            {
+                Console.WriteLine("Írd be hány számot kérjen be a program: ");
+                int darab = int.Parse(Console.ReadLine());
+                int[] tomb = new int[darab];
+                string bemenet;
+                Console.WriteLine("Írj be {0} darab számot: ", darab);
+                for (int i = 0; i < darab; i++)
+                {
+                    bemenet = Console.ReadLine();
+                    tomb[i] = int.Parse(bemenet);
+                }
+                int max = tomb[0];
+                int min = tomb[0];
+                for (int i = 0; i < darab; i++)
+                {
+                    if (tomb[i] > max)
+                    {
+                        max = tomb[i];
+                    }
+                    else if (tomb[i] < max)
+                    {
+                        min = tomb[i];
+                    }
+                }
+                Console.WriteLine("Max elem: {0}, min elem: {1}", max, min);
+            }
+            catch (System.FormatException) {
+                Console.WriteLine("Hibás adatbevitel! A program leáll");
+            }
             Console.ReadKey();
         }
     }
